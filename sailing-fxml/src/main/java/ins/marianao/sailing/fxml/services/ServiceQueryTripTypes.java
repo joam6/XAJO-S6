4a07b5b41de739170e1b223291610e546f559de6
joam6 <joseporiolam@gmail.com>
2025-02-03 20:37:27 +0100

    ServiceQueryTripTypes

---
+++ sailing-fxml/src/main/java/ins/marianao/sailing/fxml/services/ServiceQueryTripTypes.java
@@ -0,0 +1,15 @@
+package ins.marianao.sailing.fxml.services;
+
+import java.util.List;
+
+import cat.institutmarianao.sailing.ws.model.TripType;
+
+public class ServiceQueryTripTypes extends ServiceQueryBase<TripType> {
+
+	@Override
+	protected List<TripType> customCall() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
