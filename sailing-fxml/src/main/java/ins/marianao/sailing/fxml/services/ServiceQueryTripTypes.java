037b62e0c02731de179bc7fb95816886c2362521
joam6 <joseporiolam@gmail.com>
2025-02-03 20:44:36 +0100

    Update QueryTripType

--- sailing-fxml/src/main/java/ins/marianao/sailing/fxml/services/ServiceQueryTripTypes.java
+++ sailing-fxml/src/main/java/ins/marianao/sailing/fxml/services/ServiceQueryTripTypes.java
@@ -5,7 +5,27 @@ import java.util.List;
 import cat.institutmarianao.sailing.ws.model.TripType;
 
 public class ServiceQueryTripTypes extends ServiceQueryBase<TripType> {
-
+	
+	private int Id;
+	private String Category;
+	private String Departures;
+	private String Description;
+	private int Duration;
+	private int Places;
+	private int Price;
+	private String Title;
+	
+	public ServiceQueryTripTypes(int Id, String Category, String Description, int Duration, int Places, int Price, String Title) {
+		this.Id = Id;
+		this.Category = Category;
+		this.Departures = Departures;
+		this.Description = Description;
+		this.Duration = Duration;
+		this.Places = Places;
+		this.Price = Price;
+		this.Title = Title;
+	}
+	
 	@Override
 	protected List<TripType> customCall() throws Exception {
 		// TODO Auto-generated method stub
